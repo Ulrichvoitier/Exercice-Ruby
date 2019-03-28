5a2408bdaa1610f1877efcc38961a20c77382264
Ulrichvoitier <48715525+Ulrichvoitier@users.noreply.github.com>
2019-03-28 11:52:34 +0100

    Add files via upload

--- pyramide.rb
+++ pyramide.rb
@@ -1,18 +1,20 @@
-pyramide = []
-ct = etage
+pyramide = ["#"]
+
 boucle = 0
 
-puts "Combien d'étage souhaites tu?"
+puts "Salut, bienvenue dans ma super pyramide! combien d'étage souhaites tu?"
 print ">"
 etage = gets.to_i
+
+ct = etage
 
 puts "Voici la pyramide"
 loop do 
 	var2 = '#' * (etage - boucle)
-	var1 = '.' * (etage - ct)
+	var1 = ' ' * (etage - ct)
 	pyramide << var1 + var2
 	ct -= 1
-	boucle += 1 
+	boucle += 1
 	if boucle == etage 
 		break
 	end
